97517e78a83c4741e4db379120fb27037e02fae1
akeost <ake.ostmark@gmail.com>
2015-04-09 12:54:37 +0200

    Nada

--- src/se/baving/silly/SillyClass.java
+++ src/se/baving/silly/SillyClass.java
@@ -3,8 +3,9 @@ package se.baving.silly;
 public class SillyClass {
 
 	public static void main(String args[]) {
-		System.out.println("Hello Worldo");
+		System.out.println("Hello Worldoo");
 		System.out.println("Hallå världen!");
+		
 	}
 	
 }
