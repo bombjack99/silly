5f133d24ba45acbc2b5ace6edad98aba040b1237
akeost <ake.ostmark@gmail.com>
2015-05-19 15:02:31 +0200

    Updated ignore file to... - ignore directory with generated class files - idea proj file

---
+++ m/src/com/velvet/m/client/Client.java
@@ -0,0 +1,11 @@
+package com.velvet.m.client;
+
+/**
+ * Created by ake on 5/19/15.
+ */
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Starting mClient");
+        // write your code here
+    }
+}
